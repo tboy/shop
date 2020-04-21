4f6cb5d6c1a0ba7f8e2fd28dd760bb761c5dcac7
fdh <562358267@qq.com>
2020-04-21 16:39:17 +0800

    #fdg

--- pages/components/goodsItem.js
+++ pages/components/goodsItem.js
@@ -1,5 +1,5 @@
 Vue.component('Items', {
-	props: ['item'],
-	template: '<div  class="rect"><img :src="item.img" class="p_img" /><div class="bottomItem"><div class="titleTxt">{{item.title}}</div><div class="price"><div>￥{{item.price}}</div><div>￥{{item.oPrice}}</div></div></div></div>'
+	props: ['item','tap'],
+	template: '<div  @touchend="tap(item)" class="rect"><img :src="item.img" class="p_img" /><div class="bottomItem"><div class="titleTxt">{{item.title}}</div><div class="price"><div>￥{{item.price}}</div><div>￥{{item.oPrice}}</div></div></div></div>'
 })
 
